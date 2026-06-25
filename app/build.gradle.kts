e24865f59c28f1151a4ed1c01f975d947926a9e5
moemoli <moli@imoli.moe>
2026-06-25 13:50:06 +0800

    fix: sign

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     signingConfigs {
         create("release") {
 
-            storeFile = file(System.getenv("KEYSTORE_FILE") ?: "debug.keystore")
+            storeFile = file( "keystore.jks")
             storePassword = System.getenv("KEYSTORE_PASSWORD")
             keyAlias = System.getenv("KEY_ALIAS")
             keyPassword = System.getenv("KEY_PASSWORD")
